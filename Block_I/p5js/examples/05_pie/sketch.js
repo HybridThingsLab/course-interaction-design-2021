f84997df508b6487c696939d951c41d260b166c9
Christoph Haag <christoph@lafkon.net>
2021-03-10 17:30:53 +0100

    fill a pie.

---
+++ Block_I/p5js/examples/05_pie/sketch.js
@@ -0,0 +1,34 @@
+let w = 600
+let h = 600;
+
+let seconds, milliseconds;
+
+function setup() {
+
+	  createCanvas(w,h);
+    angleMode(DEGREES);
+ 
+    noStroke();
+    fill(255,0,0);
+}
+
+function draw() {
+ 
+  background(255);
+
+  milliseconds = int(millis()%60000);
+  seconds = milliseconds / 1000;
+//seconds = int(milliseconds / 1000);
+
+
+  push();
+  translate(w/2,h/2);
+  rotate(-90);
+  fill(map(int(seconds),0,60,0,255),map(int(seconds),0,60,255,0),0);
+  //fill(150);
+  arc(0,0, w, w, 0, seconds*6, PIE);
+  //fill(map(int(seconds),0,60,0,255),map(int(seconds),0,60,255,0),0);
+  //arc(0,0, w, w, int(seconds * 6) - int(seconds*6)%10, seconds*6, PIE);
+  pop();
+
+}
