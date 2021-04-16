8f384e8dc59f11ca539cf94f6491cf10d6a68ba1
Christoph Haag <christoph@lafkon.net>
2021-04-16 10:45:36 +0200

    use https://p5js.org/reference/#/p5/resizeCanvas to prevent error
    Uncaught TypeError: Cannot set property canvas of #<CanvasRenderingContext2D> which has only a getter
        at c.default.Renderer2D.f.default.Element.size (p5.min.js:3)
        at window.onresize (sketch.js:23)
    f.default.Element.size @ p5.min.js:3
    window.onresize @ sketch.js:23

--- Block_II/p5js/examples/01_responsive/sketch.js
+++ Block_II/p5js/examples/01_responsive/sketch.js
@@ -2,7 +2,7 @@ var w = window.innerWidth;
 var h = window.innerHeight;  
 
 function setup() {
-  canvas=createCanvas(w, h);
+  canvas=createCanvas(w,h);
 }
 
 function draw() {
@@ -11,14 +11,14 @@ function draw() {
   noStroke();
   fill(200, 200, 200);
   // uses global variables for width and height
-  rect(20,20,w-40,h-40);
+  rect(50,50,w-100,h-100);
   fill(255,0,0);
   ellipse(w/2,h/2,w/1.5,w/1.5)
   }
 
-window.onresize = function() {
-  // assigns new values for width and height variables
-  w = window.innerWidth;
-  h = window.innerHeight;  
-  canvas.size(w,h);
+function windowResized() {
+    // assigns new values for width and height variables
+    w = window.innerWidth;
+    h = window.innerHeight;  
+    resizeCanvas(w,h);
 }
