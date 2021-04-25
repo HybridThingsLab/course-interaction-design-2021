60658bdb436abfb52ab7994fbea2dcf12b8b3f8f
Andreas Muxel <login@andreasmuxel.com>
2021-04-25 22:07:26 +0200

    autoformat 'beautify'

--- Block_II/p5js/examples/01_responsive/sketch.js
+++ Block_II/p5js/examples/01_responsive/sketch.js
@@ -1,24 +1,24 @@
 var w = window.innerWidth;
-var h = window.innerHeight;  
+var h = window.innerHeight;
 
 function setup() {
-  canvas=createCanvas(w,h);
+  canvas = createCanvas(w, h);
 }
 
 function draw() {
-  background(255,255,128);
+  background(255, 255, 128);
   // A rectangle
   noStroke();
   fill(200, 200, 200);
   // uses global variables for width and height
-  rect(50,50,w-100,h-100);
-  fill(255,0,0);
-  ellipse(w/2,h/2,w/1.5,w/1.5)
-  }
+  rect(50, 50, w - 100, h - 100);
+  fill(255, 0, 0);
+  ellipse(w / 2, h / 2, w / 1.5, w / 1.5)
+}
 
 function windowResized() {
-    // assigns new values for width and height variables
-    w = window.innerWidth;
-    h = window.innerHeight;  
-    resizeCanvas(w,h);
+  // assigns new values for width and height variables
+  w = window.innerWidth;
+  h = window.innerHeight;
+  resizeCanvas(w, h);
 }
