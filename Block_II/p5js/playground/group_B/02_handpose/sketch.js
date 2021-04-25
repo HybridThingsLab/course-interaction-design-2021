3dec742df01ca47078ccf82b206dd9109d0e79ab
Andreas Muxel <login@andreasmuxel.com>
2021-04-25 21:15:39 +0200

    examples PoseNet, Handpose, Facemesh added

---
+++ Block_II/p5js/playground/group_B/02_handpose/sketch.js
@@ -0,0 +1,127 @@
+// webcam
+let video;
+
+// handpose
+let handpose;
+let poses = [];
+
+
+function setup() {
+
+  // canvas
+  const canvas = createCanvas(640, 480);
+  canvas.parent('canvas');
+
+  // init webcam
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  // init handpose, see also https://google.github.io/mediapipe/solutions/hands.html
+
+  // options
+  const options = {
+    flipHorizontal: true, // boolean value for if the video should be flipped, defaults to false
+    maxContinuousChecks: Infinity, // How many frames to go without running the bounding box detector. Defaults to infinity, but try a lower value if the detector is consistently producing bad predictions.
+    detectionConfidence: 0.8, // Threshold for discarding a prediction. Defaults to 0.8.
+    scoreThreshold: 0.75, // A threshold for removing multiple (likely duplicate) detections based on a "non-maximum suppression" algorithm. Defaults to 0.75
+    iouThreshold: 0.3, // A float representing the threshold for deciding whether boxes overlap too much in non-maximum suppression. Must be between [0, 1]. Defaults to 0.3.
+  }
+  handpose = ml5.handpose(video, options, modelReady);
+
+  select('#output').html('... loading model');
+
+  // detect if new pose detected and call 'gotResultModel'
+  handpose.on('predict', gotResultsModel);
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+
+}
+
+
+function draw() {
+  // clear background
+  background(0);
+
+  // show video (flipped)
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, width, height);
+  pop();
+
+  // show results of handpose
+  drawKeypoints();
+  drawSkeleton();
+
+}
+
+
+// model ready
+function modelReady() {
+  select('#output').html('model loaded');
+}
+
+// results of current model (p.ex. PoseNet, handpose, facemesh...)
+function gotResultsModel(result) {
+  poses = result;
+  // just update optimized input data if new input data available
+  if (poses.length > 0) {
+    optimizedInputData = poses[0].landmarks;
+    // console.log(optimizedInputData);
+  }
+}
+
+////////////////////////////
+// Visualization handpose //
+////////////////////////////
+
+// draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < poses.length; i++) {
+    const pose = poses[i];
+
+    // have a detailed look in your console
+    // console.log(pose); 
+
+    for (let j = 0; j < pose.landmarks.length; j += 1) {
+      const keypoint = pose.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
+  }
+}
+
+// draw the skeletons
+function drawSkeleton() {
+
+  for (let i = 0; i < poses.length; i++) {
+    //onst pose = poses[i];
+    let annotations = poses[0].annotations;
+    stroke(0, 255, 0);
+    for (let j = 0; j < annotations.thumb.length - 1; j++) {
+      line(annotations.thumb[j][0], annotations.thumb[j][1], annotations.thumb[j + 1][0], annotations.thumb[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.indexFinger.length - 1; j++) {
+      line(annotations.indexFinger[j][0], annotations.indexFinger[j][1], annotations.indexFinger[j + 1][0], annotations.indexFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.middleFinger.length - 1; j++) {
+      line(annotations.middleFinger[j][0], annotations.middleFinger[j][1], annotations.middleFinger[j + 1][0], annotations.middleFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.ringFinger.length - 1; j++) {
+      line(annotations.ringFinger[j][0], annotations.ringFinger[j][1], annotations.ringFinger[j + 1][0], annotations.ringFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.pinky.length - 1; j++) {
+      line(annotations.pinky[j][0], annotations.pinky[j][1], annotations.pinky[j + 1][0], annotations.pinky[j + 1][1]);
+    }
+
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.thumb[0][0], annotations.thumb[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.indexFinger[0][0], annotations.indexFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.middleFinger[0][0], annotations.middleFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.ringFinger[0][0], annotations.ringFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.pinky[0][0], annotations.pinky[0][1]);
+  }
+
+}
---
+++ Block_II/p5js/playground/group_B/03_facemesh/sketch.js
@@ -0,0 +1,115 @@
+// webcam
+let video;
+
+// facemesh
+let facemesh;
+let mesh = [];
+let keypoints = [];
+
+
+function setup() {
+
+  // canvas
+  const canvas = createCanvas(640, 480);
+  canvas.parent('canvas');
+
+  // init webcam
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  // init facemesh, see also https://google.github.io/mediapipe/solutions/face_mesh.html
+  // flip horizontal still not working
+  facemesh = ml5.facemesh(video, {
+    flipHorizontal: false
+  }, modelReady);
+  select('#output').html('... loading model');
+
+  // detect if new facemesh detected and call 'gotResultModel'
+  facemesh.on('predict', gotResultsModel);
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+
+}
+
+
+function draw() {
+  // clear background
+  background(0);
+
+  // show video (flipped)
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, width, height);
+  pop();
+
+  // show results of facemesh
+  drawBox();
+  drawMesh();
+
+}
+
+
+// model ready
+function modelReady() {
+  select('#output').html('model loaded');
+}
+
+// results of current model (p.ex. PoseNet, handpose, facemesh...)
+function gotResultsModel(result) {
+  // just update optimized input data if new input data available
+  if (result.length > 0) {
+    mesh = result;
+
+    // have a detailed look in your console
+    //console.log(result[0]);
+
+    keypoints = result[0].scaledMesh; // just first face
+    //keypoints = result[0].mesh; // if x,y,z position of face is not important use this instead
+
+    // just use x and y coordinate
+    inputData = keypoints.map(p => [p[0], p[1]]);
+  }
+}
+
+////////////////////////////
+// Visualization facemesh //
+////////////////////////////
+
+function drawBox() {
+  for (let i = 0; i < mesh.length; i += 1) {
+
+    // get position and size bounding box
+    let boundingBox = mesh[i].boundingBox;
+    let posX = boundingBox.topLeft[0][0];
+    let posY = boundingBox.topLeft[0][1];
+    let w = boundingBox.bottomRight[0][0] - posX;
+    let h = boundingBox.bottomRight[0][1] - posY;
+
+    // flip position x (quick & dirty)
+    posX = width - w - posX;
+
+    // draw bounding box
+    noFill();
+    stroke(0, 255, 0);
+    rect(posX, posY, w, h);
+
+  }
+}
+
+function drawMesh() {
+  for (let i = 0; i < mesh.length; i += 1) {
+
+    // draw keypoints
+    for (let j = 0; j < keypoints.length; j += 1) {
+      let [x, y] = keypoints[j];
+      // flip position x
+      x = width - x;
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(x, y, 3, 3);
+    }
+  }
+}
